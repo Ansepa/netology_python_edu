bc4ebe47e9c7c87058ac82fd1b29de484f5f204d
Anastasia Panova <anastasi8panova@yandex.ru>
2025-11-08 20:42:02 +0300

    homework 7

---
+++ homework_7/task2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn  as sns
+import matplotlib.pyplot as plt
+df = pd.read_csv("data.csv",nrows=15)
+
+sns.regplot(data=df,x="age",y="bill")
+plt.title("Зависимость суммы счёта от возраста клиента")
+plt.xlabel("Возраст клиента")
+plt.ylabel("Сумма счёта")
+plt.show()
